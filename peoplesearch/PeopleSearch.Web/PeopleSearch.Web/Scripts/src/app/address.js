a21dfde2a38c396959a5bde18c0f3b571cf6d487
Chase Hanson <chase@iactionable.com>
2017-08-29 18:29:30 -0600

    Fix project file and remove extra js/map files

--- peoplesearch/PeopleSearch.Web/PeopleSearch.Web/Scripts/src/app/address.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var Address = (function () {
-    function Address() {
-    }
-    return Address;
-}());
-exports.default = Address;
-//# sourceMappingURL=address.js.map
